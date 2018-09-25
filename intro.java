ced2a60fc136f93cebe08adf3143ce3da6de4b8e
yu <nyalpaca0707@gmail.com>
2018-09-26 02:35:42 +0900

    最初の課題

---
+++ intro.java
@@ -0,0 +1,16 @@
+
+public class intro {
+	
+	public static void main(String[] args) {
+		Chara a = new Chara(100, 30, "勇者");
+		a.speak();
+		
+		Chara b = new Chara(40, 100, "魔術師");
+		b.speak();
+		
+		Chara c = new Chara(10, 10, "一般人");
+		c.speak();
+		
+	}
+
+}
